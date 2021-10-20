eb97f3f18eb152a33513aab35e9af5358533d6f4
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-10-21 00:43:02 +0300

    merge it okwai !!

--- baseapp/models.py
+++ baseapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Email(models.Model):
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.email
--- baseapp/urls.py
+++ baseapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('save-email',views.saveEmail.as_view(),name='saveEmail')
 ]
