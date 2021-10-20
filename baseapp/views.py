3ab437aa919ab17c897240ae8046e71d50735000
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2021-10-21 01:02:42 +0300

    comm 3

--- baseapp/views.py
+++ baseapp/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Email
+from django.urls import reverse_lazy
 # Create your views here.
 def home(request):
     return render(request,'baseapp/home.html',{})
+
+class saveEmail(generic.CreateView):
+    model=Email
+    fields='__all__'
+    template_name='baseapp/home.html'
+    success_url=reverse_lazy('home')
